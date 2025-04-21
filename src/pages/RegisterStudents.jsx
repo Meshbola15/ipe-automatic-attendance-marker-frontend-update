e740ab7a33910092d0edde09a4152cb83d2a8a2f
Lanzz <lanzzd191@gmail.com>
2025-04-21 08:41:02 +0100

    update

--- src/pages/RegisterStudents.jsx
+++ src/pages/RegisterStudents.jsx
@@ -58,7 +58,7 @@ const RegisterStudent = () => {
         });
 
       if (labeledDescriptors.length > 0) {
-        const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.35);
+        const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.44);
         const bestMatch = faceMatcher.findBestMatch(newStudentFaceData);
 
         if (bestMatch.label !== 'unknown') {
